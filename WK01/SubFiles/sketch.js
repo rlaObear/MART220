0f9e791fc806f54a04cde8fc8295c879266a7467
rlaObear <rhonda1.aubert@umontana.edu>
2024-01-29 10:57:48 -0700

    setting up

--- WK01/SubFiles/sketch.js
+++ WK01/SubFiles/sketch.js
@@ -1,20 +1,10 @@
+
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(300, 500);
+}
+function draw() {
+  background(220);
+  // basic shapes
+  circle(35,55,25);
   
-  function draw() {
-    background(255);
-  
-    // Draw the pie slice
-    fill(255, 204, 0); // Yellowish color for the pie
-    stroke(0); // Black outline
-    strokeWeight(2);
-    beginShape();
-    vertex(200, 200);
-    arc(200, 200, 200, 200, PI, -PI/4, PIE);
-    endShape(CLOSE);
-  
-    // Draw whipped cream on top
-    fill(255); // White color for whipped cream
-    noStroke();
-    ellipse(200, 140, 120, 40);
+}
