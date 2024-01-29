e7964294829e841d097d4dc08f8fa9bb50da39cc
rlaObear <rhonda1.aubert@umontana.edu>
2024-01-29 11:53:09 -0700

    working on ChatGPT food

--- WK01/SubFiles/sketchChatJPT.js
+++ WK01/SubFiles/sketchChatJPT.js
@@ -3,19 +3,30 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(220);
 
-  // Draw the pie slice
-  fill(255, 204, 0); // Yellowish color for the pie
-  stroke(0); // Black outline
-  strokeWeight(2);
+  // Draw plate
+  fill(200);
+  ellipse(width / 2, height / 2, 300, 200);
+
+  // Draw pie
+  fill(255, 165, 79); // Color for the pie (you can adjust this)
   beginShape();
-  vertex(200, 200);
-  arc(200, 200, 200, 200, PI, -PI/4, PIE);
+  vertex(width / 2, height / 2); // Center of the pie
+  for (let i = 0; i < PI; i += PI / 180) {
+    let x = width / 2 + cos(i) * 150;
+    let y = height / 2 + sin(i) * 100;
+    vertex(x, y);
+  }
   endShape(CLOSE);
 
-  // Draw whipped cream on top
+  // Draw whipped cream
   fill(255); // White color for whipped cream
-  noStroke();
-  ellipse(200, 140, 120, 40);
+  beginShape();
+  for (let i = 0; i < PI; i += PI / 180) {
+    let x = width / 2 + cos(i) * 130;
+    let y = height / 2 + sin(i) * 80;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
 }
