2829872254f2fa0695102024e52dccfc13af618c
rlaObear <rhonda1.aubert@umontana.edu>
2024-01-30 21:35:58 -0700

    Putting WK01 in WK02

---
+++ WK01/SubFiles copy/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //pizza crust outer circle//
+   fill (252,215,135);
+  circle(200,200,200);
+  //pizza sauce and center circle//
+   fill (251,135,95); 
+  circle(200,200,175);
+  //pepparoni on pizza
+   fill(252, 79, 59 )
+  circle(210,145,25);
+  circle(245,175,25);
+  circle(180,200,25);
+  circle(140,210,25);
+  circle(160,145,25);
+  circle(190,250,25);
+  circle(240,230,25);
+  //white dots in pepparoni//
+   fill(251,225,222)
+  circle(215,145,5);
+  circle(245,180,5);
+  circle(175,200,5);
+  circle(140,215,5);
+  circle(160,140,5);
+  circle(185,245,5);
+  circle(240,225,5);
+}
