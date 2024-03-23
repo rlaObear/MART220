b2e5fdf7ac01d812be4ef79fd22d147e4fb5a0f7
rlaObear <rhonda1.aubert@umontana.edu>
2024-03-22 19:43:48 -0600

    adding add on

--- WK08/js/myImage.js
+++ WK08/js/myImage.js
@@ -36,8 +36,8 @@ class myImage {
     }
 
     checkCollision(x2, y2, w2, h2) {
-
-        if (
+  //hit = collideRectRect(this.x - this.w / 2 ,  this.y-this.h/4 - h2, this.w,this.h,x2.y2.w2.h2);
+       if (
             this.x - this.w / 2 < x2 + w2 / 2 &&
             this.x-this.w/4 + w2 / 2 > x2 - this.w / 2 &&
             this.y-this.h/4 - h2 / 2 < y2 + this.h / 2 &&
